265ce5319949381a02164e23edce1e1721d734e6
priyak-sdei <priya.kumari@smartdatainc.net>
2022-11-22 10:21:38 +0530

    Code

--- src/actions/auth/index.js
+++ src/actions/auth/index.js
@@ -1,9 +1,39 @@
 import GLOBALS from '../../constants';
-const {actionType} = GLOBALS;
+const { actionType } = GLOBALS;
 export function login(data) {
   return async (dispatch, getState) => {
     dispatch({
       type: 'increment',
+      payload: data,
+    });
+  };
+}
+
+export function addLoginUserData(data) {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: 'addLoginData',
+      payload: data,
+    });
+  };
+}
+
+
+
+export function addition() {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: 'add',
+      payload: { num1: 5, num2: 4 },
+    });
+  };
+}
+
+
+export function saveEvent(data) {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: 'addEvent',
       payload: data,
     });
   };
--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,7 +1,7 @@
-import {Button, View, Text} from 'react-native';
+import { Button, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import React, {useState, useEffect, useCallback} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import * as AppActions from '../../../actions';
 function Login(props) {
   let [userCount, setUserCount] = useState(0);
@@ -10,25 +10,28 @@ function Login(props) {
 
   /**Adding event On Mount */
   useEffect(() => {
-    dispatch(AppActions.login(Math.random()));
+    dispatch(AppActions.saveEvent([{ name: 121211 }]))
+    // dispatch(AppActions.addLoginUserData({ name: 'Priya', action: "Demo react" }))
+    // dispatch(AppActions.addition())
   }, []);
 
   useEffect(() => {
     setTimeout(() => {
-      dispatch(AppActions.login(Math.random()));
+      dispatch(AppActions.saveEvent([{ name: 1 }, { name: 2 }]));
     }, 4000);
   }, []);
 
   /*****dding listener  everytime props.x changes ***/
   useEffect(() => {
-    setUserCount('change' + Math.random());
-    console.log(props, 'props...', authReducer);
-    return () => {
-      // removing the listener when props.x changes
-    };
-  }, [authReducer.value]);
+    console.log(authReducer, "authReducer event_list changes");
+    // setUserCount('change' + Math.random());
+    // console.log(props, 'props...', authReducer);
+    // return () => {
+    //   // removing the listener when props.x changes
+    // };
+  }, [authReducer.event_list]);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>
         Logins screen {authReducer.value} {userCount}
       </Text>
