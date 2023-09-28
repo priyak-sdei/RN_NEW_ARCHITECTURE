8757e4d73d29dc8b44740a3c24cfb852fd6e1147
Priya Kumari <priya.kumari@Priyas-MacMiniM2Pro.local>
2023-09-28 18:26:24 +0530

    Test

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,6 +5,6 @@ module.exports = {
     singleQuote: true, //Determines whether single quotes (') or double quotes (") are used for string literals.
     trailingComma: 'all',
     printWidth: 100, //Specifies the maximum line length before code is wrapped or truncated.
-    tabWidth: 2, // Defines the number of spaces or tabs per indentation level.
-    semi: false, //Controls whether or not semicolons are added at the end of statements.
-}
+    tabWidth: 4, // Defines the number of spaces or tabs per indentation level.
+    semi: true, //Controls whether or not semicolons are added at the end of statements.
+};
--- src/App.tsx
+++ src/App.tsx
@@ -5,111 +5,123 @@
  * @format
  */
 
-import React from 'react'
-import type {PropsWithChildren} from 'react'
+import React from 'react';
+import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native'
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Dimensions,
+} from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
-import GLOBALS from '@constants/index'
-const {FONTS} = GLOBALS
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import GLOBALS from '@constants/index';
+
+const {FONTS} = GLOBALS;
 type SectionProps = PropsWithChildren<{
-  title: string
-}>
+    title: string;
+}>;
 
 function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}sfsdf
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  )
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+            <Text
+                style={[
+                    styles.sectionTitle,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}>
+                {title}
+                sfsdf
+            </Text>
+            <Text
+                style={[
+                    styles.sectionDescription,
+                    {
+                        color: isDarkMode ? Colors.light : Colors.dark,
+                    },
+                ]}>
+                {children}
+            </Text>
+        </View>
+    );
 }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
+    const isDarkMode = useColorScheme() === 'dark';
+    function add(x: string) {
+        const a = '1';
+        const b = 2;
+        if (a == x) {
+            console.log('true');
+        } else {
+            console.log('gogd');
+        }
+    }
+    add('122');
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back
-            to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">Read the docs to discover what to do next:</Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+                <Header />
+                <View
+                    style={{
+                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+                    }}>
+                    <Section title="Step One">
+                        <Text style={styles.highlight}>App.tsx</Text> to change this screen and then
+                        come back to see your edits. d d d fgwgfjwf fwfwe
+                    </Section>
+                    <Section title="See Your Changes">
+                        <ReloadInstructions />
+                    </Section>
+                    <Section title="Debug">
+                        <DebugInstructions />
+                    </Section>
+                    <Section title="Learn More">Read the docs to discover what to do next:</Section>
+                    <LearnMoreLinks />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionDescription: {
-    fontSize: 18,
-    fontWeight: '400',
-    marginTop: 8,
-  },
-  sectionTitle: {
-    fontFamily: FONTS.NUNITO_BOLD,
-    fontSize: 24,
-    // fontWeight: '600',
-  },
-})
+    highlight: {
+        fontWeight: '700',
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionDescription: {
+        fontSize: 18,
+        fontWeight: '400',
+        marginTop: 8,
+    },
+    sectionTitle: {
+        fontFamily: FONTS.NUNITO_BOLD,
+        fontSize: 24,
+    },
+});
 
-export default App
+export default App;
