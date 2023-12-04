f0cc0761dd8fe996db1f77bee5ecfaca9ff12113
Priya Kumari <priya.kumari@Priyas-MacMiniM2Pro.local>
2023-12-04 11:55:34 +0530

    Code

--- src/components/common/ParentContainer/ParentContainer.tsx
+++ src/components/common/ParentContainer/ParentContainer.tsx
@@ -1,28 +1,27 @@
-import React from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
-import { Loader } from "../..";
-import { styles } from "./ParentContainer.styles";
+import React from 'react';
+import {KeyboardAvoidingView, Platform, View} from 'react-native';
+import {Loader} from '../..';
+import {styles} from './ParentContainer.styles';
 
 export const ParentContainer = props => {
-    const { isLoading = true, children, style, keyboardAvoidingView, ...attributes } = props;
+    const {isLoading = true, children, style, keyboardAvoidingView, ...attributes} = props;
     return (
         <View style={[styles.parent, style]} {...attributes}>
             {/* statusBar if needed   */}
             {/* <StatusBar barStyle={Platform.OS === "ios" ? "dark-content" : "default"} /> */}
-            {isLoading && <View style={[styles.Loader, style]}>
+            {/* {isLoading && <View style={[styles.Loader, style]}>
                 <Loader loading={isLoading ? isLoading : false} />
-            </View>}
-            {keyboardAvoidingView === true
-                ? (
-                    <KeyboardAvoidingView
-                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                        keyboardVerticalOffset={170}
-                        style={styles.keyboardAvoidingContainer}
-                    >
-                        {children}
-                    </KeyboardAvoidingView>
-                )
-                : (children)}
+            </View>} */}
+            {keyboardAvoidingView === true ? (
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    keyboardVerticalOffset={170}
+                    style={styles.keyboardAvoidingContainer}>
+                    {children}
+                </KeyboardAvoidingView>
+            ) : (
+                children
+            )}
         </View>
     );
 };
--- src/redux/service/clientApi.ts
+++ src/redux/service/clientApi.ts
@@ -1,32 +1,18 @@
-
 import urlMapping from '@constants/urlMapping';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-export const clientApi =createApi({
-            baseQuery: fetchBaseQuery({
-                baseUrl: urlMapping.BASE_URL,
-                // credentials: 'include',
-                // timeout: 5000,
-                prepareHeaders: async (headers, { getState, endpoint }) => {
-                    /**
-                     * get the token from redux store directly using the getState function
-                     */
-                    // const userToken = getState()?.userSlice.token;
-                    // if (userToken) {
-                    //     headers.set('Authorization', `${userToken}`);
-                    //     headers.set('Content-Type', 'application/json');
-                    // }
-                    // else {
-                    //     headers.set('Content-Type', 'application/json');
-                    // }
-                    // return headers;
-                    /**
-                     * Endpoint are the same as function name e.g Login
-                     */
-                    if (endpoint === 'Login') {
-                        // return different header here
-                    }
-                },
-            }),
-            endpoints: (builder) => ({}),
-        });
+export const clientApi = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: urlMapping.BASE_URL,
+        // credentials: 'include',
+        timeout: 10000,
+        prepareHeaders: async (headers, {getState, endpoint}) => {
+            headers.set('Content-Type', 'application/json');
+            return headers;
+            /* Conditional headers based on endpoint  */
+            if (endpoint === 'Login') {
+            }
+        },
+    }),
+    endpoints: builder => ({}),
+});
